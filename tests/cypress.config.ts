515e93dd4775bba44a9bf569629a42f305aad8e7
Florent BOURASSE <fbourasse@jahia.com>
2026-05-13 11:39:20 +0200

    test: increase Cypress defaultCommandTimeout to 10s and update provisioning
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     videosFolder: './results/videos',
     viewportWidth: 1366,
     viewportHeight: 768,
+    defaultCommandTimeout: 10000,
     watchForFileChanges: false,
     e2e: {
         setupNodeEvents(on, config) {
